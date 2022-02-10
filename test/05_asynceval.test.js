e953110b9432b821cdef55142e2adcc31a5b5094
Dethe Elza <dethe@livingcode.org>
2022-02-09 20:44:08 -0800

    more attempts to get asynceval working before realizing it was the wrong approach

--- test/05_asynceval.test.js
+++ test/05_asynceval.test.js
@@ -1,45 +1,52 @@
 import assert from "assert";
 import Env from "../engine/env.js";
 
-let env, list;
-
-let resolve = async list => {
+let resolve = async function (list) {
   let env = new Env();
-  let l = list.slice();
+  let l = Array.from(list);
   while (l.length) {
-    await l.shift()();
+    await l.shift();
   }
+  return list;
 };
+
+let list;
 
 describe("test async list processing", function () {
   beforeEach(function () {
-    env = new Env();
-    let value1 = async function() {
+    let env = new Env();
+    let value1 = async function (env) {
       return new Promise(resolve => {
         setTimeout(() => {
+          console.log("pushing value 1");
           env.push("value1", 1);
           resolve(env);
         });
       });
     };
 
-    let value2 = async env => {
+    let value2 = async function (env) {
       return new Promise(resolve => {
+        console.log("pushing value 2");
         env.push("value2", 3 * env.get("value1"));
         setTimeout(() => env);
         resolve(env);
       });
     };
 
-    let value3 = async env => {
-      env.push("value3", 5 * env.get("value2"));
+    let value3 = async function (env) {
+      return new Promise(resolve => {
+        env.push("value3", 5 * env.get("value2"));
+        resolve(env);
+      });
     };
-    list = [value1, value2, value3];
+    list = [value1(env), value2(env), value3(env)];
   });
   describe("test resolve", function () {
     it("get expected values", async function () {
-      await resolve(list);
-      assert.deepEqual(env.current(), { value1: 1, value2: 3, value3: 15 });
+      let [p, q, r] = await resolve(list);
+      console.log(`r: ${r}`);
+      assert.deepEqual(r.current(), { value1: 1, value2: 3, value3: 15 });
     });
   });
 });
