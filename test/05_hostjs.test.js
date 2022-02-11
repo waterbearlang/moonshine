8768c959ec435c09337a555db26924fcbeb6db6a
Dethe Elza <dethe@livingcode.org>
2022-02-10 17:02:27 -0800

    getting the code and tests are passing. Calling it done.

--- test/05_hostjs.test.js
+++ test/05_hostjs.test.js
@@ -16,7 +16,7 @@ const testdata = `unit Built-ins [
     define context loop over (list:TypeAList) (action:BlockList)[
       // needs locals for item and index
       // returns a new list
-      hosted returns list:TypeBList[@
+      hosted returns results:TypeBList[@
         let results;
         for (let index=0; index < list.length; index++){
           let item = list[index];
@@ -43,22 +43,69 @@ describe("Test host language escaping", function () {
       assert.equal(ast.units.length, 1);
     });
   });
-  describe("test library", function(){
+  describe("test library", function () {
     let library;
-    it("library should exist", function(){
+    it("library should exist", function () {
       library = ast.units[0].libraries[0];
       assert.ok(library);
     });
-    it("library should have 3 block definitions", function(){
+    it("library should have 3 block definitions", function () {
       assert.equal(library.blockDefs.length, 3);
     });
   });
-  describe("test BlockDef", function(){
+  describe("test BlockDef", function () {
     let blockDef;
-    it("first blockDef should be type BlockDef", function(){
+    it("first blockDef should be type BlockDef", function () {
       blockDef = ast.units[0].libraries[0].blockDefs[0];
       assert.ok(blockDef);
       assert.equal(blockDef.type, "BlockDef");
+    });
+    it("blockdef should have returns", function () {
+      assert.equal(blockDef.returns.name, "elapsed");
+      assert.equal(blockDef.returns.type, "Number");
+    });
+  });
+  describe("test BlockDef hosted", function () {
+    let hosted;
+    it("first step should be of type Hosted", function () {
+      hosted = ast.units[0].libraries[0].blockDefs[0].steps[0];
+      assert.ok(hosted);
+      assert.equal(hosted.type, "Hosted");
+    });
+    it("hosted should have return type", function () {
+      assert.equal(hosted.returns.name, "elapsed");
+      assert.equal(hosted.returns.type, "Number");
+    });
+    it("hosted should have code", function () {
+      assert.equal(hosted.code.trim(), "return Math.PI;");
+    });
+  });
+  describe("test TriggerDef hosted", function () {
+    let hosted;
+    it("first step should be of type HostedNoReturns", function () {
+      hosted = ast.units[0].libraries[0].blockDefs[1].steps[0];
+      assert.ok(hosted);
+      assert.equal(hosted.type, "HostedNoReturns");
+    });
+    it("hosted should have code", function () {
+      assert.equal(hosted.code.trim(), "steps.forEach(step => step.run());");
+    });
+  });
+  describe("test ContextDef hosted", function () {
+    let hosted;
+    it("first step should be of type Hosted", function () {
+      hosted = ast.units[0].libraries[0].blockDefs[2].steps[0];
+      assert.ok(hosted);
+      assert.equal(hosted.type, "Hosted");
+    });
+    it("hosted should have return type", function () {
+      assert.equal(hosted.returns.name, "results");
+      assert.equal(hosted.returns.type, "TypeBList");
+    });
+    it("hosted should have code", function () {
+      assert.ok(hosted.code.trim().startsWith("let results;"));
+      assert.ok(hosted.code.trim().endsWith("}"));
+      assert.equal(hosted.code.split("\n").length, 5);
     });
   });
 });
